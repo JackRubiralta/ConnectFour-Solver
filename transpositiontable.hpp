e2fbdb5245ba33d7b671b2fbbfdd758a80808c0a
JackRubiralta <75038871+JackRubiralta@users.noreply.github.com>
2022-03-26 17:39:51 +0100

    Delete transpositiontable.hpp

--- transpositiontable.hpp
+++
@@ -1,4 +0,0 @@
-// would store a entry and have a finite size
-// if overrun would remove the oldest entry and replace it with the new one.
-// favoring storage of the most recently explored nodes.
-// 
